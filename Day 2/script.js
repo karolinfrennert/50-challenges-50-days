f35517613e55a2d1d8c75353e2e91d3677baa3a6
Karolin Frennert <karolinfrennert@Karolins-MBP.RockRouter>
2020-12-06 22:31:10 +0100

    Day 2 working

---
+++ Day 2/script.js
@@ -0,0 +1,48 @@
+const progress = document.querySelector("#progress")
+const prev = document.querySelector("#prev")
+const next = document.querySelector("#next")
+const circle = document.querySelectorAll(".circle")
+
+let currentActive = 1
+
+next.addEventListener('click', () => {
+    currentActive++
+    if(currentActive > circle.length){
+        currentActive = circle.length
+    }
+
+    update()
+})
+
+prev.addEventListener('click', () => {
+    currentActive--
+    if(currentActive < 1 ){
+        currentActive = 1
+    }
+    update()
+    
+})
+
+function update(){
+    circle.forEach((circles, index) => {
+        if(index < currentActive) {
+            circles.classList.add("active")
+        } else {
+            circles.classList.remove("active")
+        }
+    })
+
+    const actives = document.querySelectorAll(".active")
+    
+    progress.style.width = ((actives.length -1) / (circle.length -1)) * 100 + "%"
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if(currentActive === circle.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
+
+}
